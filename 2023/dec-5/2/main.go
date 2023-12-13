aab8c54d0a7cd2ef2481b1517ccc5ef17d04f238
jeremy.osztreicher <jeremy.osztreicher@roh.org.uk>
2023-12-13 16:13:00 +0000

    Dec 5 still not working

--- 2023/dec-5/2/main.go
+++ 2023/dec-5/2/main.go
@@ -9,15 +9,14 @@ import (
 
 func main() {
 	seeds, Maps := parseInput("./input.txt")
+	fmt.Println("seeds: ", seeds)
 
-	seedLocs := []int{}
-	for _, seed := range seeds {
-		seedLocs = append(seedLocs, findLocation(seed, Maps))
-	}
+	new := processSeeds(seeds, Maps)
+	lowest := getLowest(new)
 
-	fmt.Println(seedLocs)
-
-	lowest := getLowest(seedLocs)
+	// To answer:
+	// Why am I off by 1?
+	// Why do I get 0 for the lowest location on input
 
 	fmt.Printf(`
 
@@ -35,17 +34,23 @@ func main() {
 	`, lowest)
 }
 
-type Seeds []int
+type Seeds [][2]int
 type Map [][]int
 type Maps []Map
 
-func parseInput(filepath string) ([]int, Maps) {
+func parseInput(filepath string) (Seeds, Maps) {
 
 	chunks := strings.Split(io_helpers.ReadFileString(filepath), "\n\n")
 
 	// Seeds is values after "Seeds:"
 	seeds := strings.Split(chunks[0], ":")[1]
 	seedsInt := string_helpers.ConvertSliceToInts(strings.Split(seeds, " "))
+
+	seedRange := [][2]int{}
+	for i := 0; i < len(seedsInt); i += 2 {
+		seedRange = append(seedRange, [2]int{seedsInt[i], seedsInt[i] + seedsInt[i+1]})
+	}
+
 	rest := chunks[1:]
 
 	maps := Maps{}
@@ -53,10 +58,7 @@ func parseInput(filepath string) ([]int, Maps) {
 		maps = append(maps, chunkToMap(chunk))
 	}
 
-	fmt.Println(seedsInt)
-	fmt.Println(maps)
-
-	return seedsInt, maps
+	return seedRange, maps
 }
 
 func chunkToMap(chunk string) [][]int {
@@ -75,32 +77,72 @@ func chunkToMap(chunk string) [][]int {
 	return currMap
 }
 
-func findLocation(seed int, maps Maps) int {
-	currNum := seed
-
-	for _, m := range maps {
-		// for each row in map
-		for _, row := range m {
-			dstStart := row[0]
-			srcStart := row[1]
-			rangeLen := row[2]
-			// if currNum is in range
-			if currNum >= srcStart && currNum < srcStart+rangeLen {
-				currNum = dstStart + (currNum - srcStart)
-				break
-			}
-		}
+func max(a, b int) int {
+	if a > b {
+		return a
 	}
+	return b
+}
 
-	return currNum
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
 }
 
 func getLowest(seeds Seeds) int {
-	lowest := seeds[0]
+	// fmt.Println("Finding lowest in seeds: ", seeds)
+	lowest := seeds[0][0]
+	// fmt.Println("lowest: ", lowest)
 	for _, seed := range seeds {
-		if seed < lowest {
-			lowest = seed
+		if seed[0] < lowest {
+			lowest = seed[0]
+			// fmt.Println("lowest: ", lowest)
 		}
 	}
 	return lowest
+}
+
+func processSeeds(seeds Seeds, maps Maps) Seeds {
+	new := [][2]int{}
+	// FOR EACH MAP
+	for _, m := range maps {
+
+		// fmt.Println("map: ", m)
+
+		// LOOP THROUGH EACH SEED RANGE
+		for _, seed := range seeds {
+			start, end := seed[0], seed[1]
+			for _, row := range m {
+				dstStart, srcStart, rangeLen := row[0], row[1], row[2]
+
+				// Find overlap between seed range and map range
+				overlapStart := max(start, srcStart)
+				overlapEnd := min(end, srcStart+rangeLen)
+
+				if overlapStart < overlapEnd {
+					shift := dstStart - srcStart
+					newRange := [2]int{
+						overlapStart - shift,
+						overlapEnd - shift,
+					}
+					new = append(new, newRange)
+
+					// Add seeds back to list if there is a gap between the start of the seed range and the start of the overlap
+					if overlapStart > start {
+						seeds = append(seeds, [2]int{start, overlapStart})
+					}
+					if end > overlapEnd {
+						seeds = append(seeds, [2]int{overlapEnd, end})
+					}
+					break
+				}
+				new = append(new, seed)
+			}
+		}
+
+	}
+
+	return new
 }
--- 2023/dec-5/2/main_test.go
+++ 2023/dec-5/2/main_test.go
@@ -33,44 +33,14 @@ func TestReadInput(t *testing.T) {
 
 func TestCalibrate(t *testing.T) {
 
-	// tests := []seedTest{
-	// 	{79, 81, 81, 81, 74, 78, 78},
-	// 	{14, 14, 53, 49, 42, 42, 43},
-	// 	{55, 57, 57, 53, 46, 82, 82},
-	// 	{13, 13, 52, 41, 34, 34, 35},
-	// 	// Add more tests as needed
-	// }
+	seeds, Maps := parseInput("./input.txt")
 
-	chunks := readFileChunks("./calibrate.txt")
+	seedLocs := [][2]int{}
+	for _, seed := range seeds {
+		seedLocs = append(seedLocs, findLocation(seed, Maps))
+	}
 
-	seeds := createSeeds(chunks[0])
-
-	maps := readMaps(chunks, seeds)
-
-	seeds = analyseSeedMap(seeds, maps)
-
-	// for _, test := range tests {
-	// 	if seeds[test.id].soil != test.soil {
-	// 		t.Errorf("Expected seeds[%d].soil to be %d, got %d", test.id, test.soil, seeds[test.id].soil)
-	// 	}
-	// 	if seeds[test.id].fertilizer != test.fertilizer {
-	// 		t.Errorf("Expected seeds[%d].fertilizer to be %d, got %d", test.id, test.fertilizer, seeds[test.id].fertilizer)
-	// 	}
-	// 	if seeds[test.id].water != test.water {
-	// 		t.Errorf("Expected seeds[%d].water to be %d, got %d", test.id, test.water, seeds[test.id].water)
-	// 	}
-	// 	if seeds[test.id].light != test.light {
-	// 		t.Errorf("Expected seeds[%d].light to be %d, got %d", test.id, test.light, seeds[test.id].light)
-	// 	}
-	// 	if seeds[test.id].temperature != test.temperature {
-	// 		t.Errorf("Expected seeds[%d].temperature to be %d, got %d", test.id, test.temperature, seeds[test.id].temperature)
-	// 	}
-	// 	if seeds[test.id].humidity != test.humidity {
-	// 		t.Errorf("Expected seeds[%d].humidity to be %d, got %d", test.id, test.humidity, seeds[test.id].humidity)
-	// 	}
-	// }
-
-	lowest := findLowestLocation(seeds)
+	lowest := getLowest(seedLocs)
 
 	// Expect lowest to be 35
 	if lowest != 46 {
