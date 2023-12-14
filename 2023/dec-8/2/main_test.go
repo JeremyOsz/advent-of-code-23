49e0c1ac3fed88a5cf4d9612118cfd0d6a3ccba5
jeremy.osztreicher <jeremy.osztreicher@roh.org.uk>
2023-12-14 19:53:32 +0000

    dec-8 - part 1 complete

---
+++ 2023/dec-8/2/main_test.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	io_helpers "jeremyosz/go-advent-2023/2023/helpers/io"
+	"testing"
+)
+
+func TestReadCalibrate(t *testing.T) {
+	input := io_helpers.ReadFileLines("./input.txt")
+	// Check if empty arr
+	if len(input) == 0 || input[0] == "" {
+		t.Errorf("Expected len(input) to be > 0, got %d", len(input))
+	}
+
+}
+func TestReadInput(t *testing.T) {
+	input := io_helpers.ReadFileLines("./input.txt")
+	// Check if empty arr
+	if len(input) == 0 || input[0] == "" {
+		t.Errorf("Expected len(input) to be > 0, got %d", len(input))
+	}
+}
+
+func TestCalibrate(t *testing.T) {
+	calibrate1 := getSteps("./calibrate.txt")
+
+	if calibrate1 != 2 {
+		t.Errorf("Expected calibrate1 to be 2, got %d", calibrate1)
+	}
+
+	calibrate2 := getSteps("./calibrate2.txt")
+	if calibrate2 != 6 {
+		t.Errorf("Expected calibrate2 to be 6, got %d", calibrate2)
+	}
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module jeremyosz/go-advent-2023
 
 go 1.18
+
+require golang.org/x/exp v0.0.0-20231214170342-aacd6d4b4611 // indirect
